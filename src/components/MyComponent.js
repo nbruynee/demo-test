8bb0d220b99fa5c09b0b319ac4a2ffebfc986fcb
BruyneHuu <buinghialk180303@gmail.com>
2025-04-12 16:06:26 +0700

    review DOM event, use State in React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+    state = {
+        name: 'Bruyne',
+        address: 'Ha noi'
+    }
+
+    handleOnClick(event) {
+        console.log("Clicked", event)
+    }
+
+    handleOnMouseOver(event) {
+        console.log("Hovered", event.pageX);
+    }
     // JSX: allow js write inside file html 
     render() {
         return (
             <div>
-                My first components
-                {Math.random()}
+                My name is : {this.state.name}
+                <button onClick={this.handleOnClick}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
             </div>
         )
     }
