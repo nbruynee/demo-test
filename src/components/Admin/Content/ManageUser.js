7b205d6e11c70bdac11015e31f06f4afa1040d9d
BruyneHuu <buinghialk180303@gmail.com>
2025-04-21 21:27:59 +0700

    feat: Implement delete a user functionality

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -80,6 +80,7 @@ const ManageUser = (props) => {
                     show={showModalDeleteUser}
                     setShow={setShowModalDeleteUser}
                     dataDelete={dataDelete}
+                    fetchListUser={fetchListUser}
                 />
             </div>
         </div>
--- src/components/Admin/Content/ModalDeleteUser.js
+++ src/components/Admin/Content/ModalDeleteUser.js
@@ -1,37 +1,50 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { deleteUser } from '../../../service/apiService'
+import { toast } from 'react-toastify';
 
 const ModalDeleteUser = (props) => {
-  const {show, setShow, dataDelete} = props;
+    const { show, setShow, dataDelete } = props;
 
-  const handleClose = () => setShow(false);
-  
-  const handleDeleteUser = () => {
-    alert('me')
-  } 
-  return (
-    <>
-      <Modal show={show} onHide={handleClose} backdrop="static">
-        <Modal.Header closeButton>
-          <Modal.Title>Confirm delete the user?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Are you sure to delete this user. Email: 
-            <b>
-                {dataDelete && dataDelete.email ? dataDelete.email : ""}
-            </b>
-            </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={() => handleDeleteUser()}>
-            Confirm
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+    const handleClose = () => setShow(false);
+
+    const handleDeleteUser = async () => {
+        let data = await deleteUser(dataDelete.id);
+        // console.log("check res: ", data);
+        if (data && data.EC === 0) {
+            toast.success(data.EM);
+            handleClose();
+            await props.fetchListUser()
+        }
+        if (data && data.EC !== 0) {
+            toast.error(data.EM);
+        }
+    }
+
+
+    return (
+        <>
+            <Modal show={show} onHide={handleClose} backdrop="static">
+                <Modal.Header closeButton>
+                    <Modal.Title>Confirm delete the user?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Are you sure to delete this user. Email:
+                    <b>
+                        {dataDelete && dataDelete.email ? dataDelete.email : ""}
+                    </b>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => handleDeleteUser()}>
+                        Confirm
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 }
 
 export default ModalDeleteUser;
